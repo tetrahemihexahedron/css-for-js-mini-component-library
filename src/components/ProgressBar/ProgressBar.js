aacdcfa72ad76a4cb39a8046d48b7cac562b4c51
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-06-04 14:19:38 -0600

    Add styling for medium progress bar. Needs accessible label.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,26 @@ import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value}>
+      <ValueBar value={value}></ValueBar>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: 12px;
+  border-radius: 4px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+`;
+
+const ValueBar = styled.div`
+  width: ${(p) => p.value}%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
