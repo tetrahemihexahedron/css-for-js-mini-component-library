6457509bb9b6635179814a3d1a64515ae5268d6d
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-06-04 15:47:06 -0600

    Changes going forward were made after I watched the progress bar solution video.
    Remove unused VisuallyHidden import. This is used in the solution to include the value so that screen readers will read it. I'm
    not sure it is still necessary; perhaps including 'aria-valuenow' is sufficient.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,7 +3,6 @@ import React from "react";
 import styled from "styled-components";
 
 import { COLORS } from "../../constants";
-import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
   const styles = STYLES_BY_SIZE[size];
