833aee8d82305195c336f9b2686830349c0fc230
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-06-04 20:41:21 -0600

    Add positioned icon to select, and hover styles.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,24 +9,43 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <Wrapper value={value} onChange={onChange}>
-      {children}
+    <Wrapper>
+      <SelectIcon id="chevron-down" size="24" strokeWidth="2" />
+      <SelectWrapper value={value} onChange={onChange}>
+        {children}
+      </SelectWrapper>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.select`
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectWrapper = styled.select`
   appearance: none;
 
+  position: relative;
   field-sizing: content;
 
   background-color: ${COLORS.transparentGray15};
   border: none;
   border-radius: 8px;
-  padding: 12px 16px;
-
+  padding: 12px 52px 12px 16px;
   font-size: 1rem;
-  color: ${COLORS.gray700};
+  color: inherit;
+`;
+
+const SelectIcon = styled(Icon)`
+  position: absolute;
+  right: 12px;
+  top: calc(50% - 12px);
 `;
 
 export default Select;
