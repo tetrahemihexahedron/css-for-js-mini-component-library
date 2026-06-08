c92831948244206608b2349dfc41894a2a67fe31
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-06-08 07:30:00 -0600

    First draft of IconInput styling. Needs the icon

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -7,7 +7,42 @@ import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
-  return "TODO";
+  const styles = STYLES_BY_SIZE[size];
+  return (
+    <TextInput
+      type="text"
+      placeholder={placeholder}
+      width={width}
+      style={styles}
+    />
+  );
 };
+
+const STYLES_BY_SIZE = {
+  small: {
+    "--line-size": 1 + "px",
+    "--font-size": 14 / 16 + "rem",
+  },
+  large: {
+    "--line-size": 2 + "px",
+    "--font-size": 18 / 16 + "rem",
+  },
+};
+
+const TextInput = styled.input`
+  width: ${(p) => p.width}px;
+
+  border: none;
+  border-bottom: var(--line-size) solid ${COLORS.black};
+
+  color: ${COLORS.gray700};
+  font-size: var(--font-size);
+  font-weight: 700;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
