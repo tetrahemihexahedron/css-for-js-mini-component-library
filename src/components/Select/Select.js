0a2c58e6110804c9ac0863af171020d933b9a22e
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-06-08 06:17:58 -0600

    Remove unused 'displayedValue'

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,11 +3,8 @@ import styled from "styled-components";
 
 import { COLORS } from "../../constants";
 import Icon from "../Icon";
-import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
   return (
     <Wrapper>
       <SelectIcon id="chevron-down" size="24" strokeWidth="2" />
